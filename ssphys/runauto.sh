9b64e5a16362e45f262166fe96c011b1e050873c
Leo Davis <ldav1s@yahoo.com>
2006-03-22 10:23:36 +0000

    added svn:mime-type and svn:eol-style there were several copies of commands pasted one after the other so I got rid of most of the commands.

--- ssphys/runauto.sh
+++ ssphys/runauto.sh
@@ -5,32 +5,3 @@ aclocal
 libtoolize
 autoconf
 automake -a
-./configure
-make
-#!/bin/sh
-# Run the auto tools
-set -x
-aclocal
-libtoolize
-autoconf
-automake -a
-./configure
-make
-#!/bin/sh
-# Run the auto tools
-set -x
-aclocal
-libtoolize
-autoconf
-automake -a
-./configure
-make
-#!/bin/sh
-# Run the auto tools
-set -x
-aclocal
-libtoolize
-autoconf
-automake -a
-./configure
-make
