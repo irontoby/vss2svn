e0fab0c89521363cb3973592f6b56d5013f6cb9e
Leo Davis <ldav1s@yahoo.com>
2006-03-22 10:18:00 +0000

    didn't mean to check in these files with that comment, rolling changes back out

--- ssphys/runauto.sh
+++ ssphys/runauto.sh
@@ -5,3 +5,32 @@ aclocal
 libtoolize
 autoconf
 automake -a
+./configure
+make
+#!/bin/sh
+# Run the auto tools
+set -x
+aclocal
+libtoolize
+autoconf
+automake -a
+./configure
+make
+#!/bin/sh
+# Run the auto tools
+set -x
+aclocal
+libtoolize
+autoconf
+automake -a
+./configure
+make
+#!/bin/sh
+# Run the auto tools
+set -x
+aclocal
+libtoolize
+autoconf
+automake -a
+./configure
+make
