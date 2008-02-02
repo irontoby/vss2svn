a2d24dc5e9649433e4f65fe5838f8353bcdb02c6
Toby J <irontoby@gmail.com>
2008-02-02 00:13:05 +0000

    Require version 0.08 of Text::Glob

--- script/Vss2Svn/Dumpfile/AutoProps.pm
+++ script/Vss2Svn/Dumpfile/AutoProps.pm
@@ -3,7 +3,7 @@ package Vss2Svn::Dumpfile::AutoProps;
 use warnings;
 use strict;
 use Config::Ini;
-use Text::Glob;
+use Text::Glob 0.08;
 
 ###############################################################################
 #  new
@@ -17,21 +17,20 @@ sub new {
     $self->{entries} = ();
 
     my ($enabled) = $config->get (['miscellany', 'enable-auto-props']);
-    if (defined $enabled && $enabled eq "yes")
-    {
+    if (defined $enabled && $enabled eq "yes") {
         my $autoprops_list = $config->get (['auto-props']);
 
-	# see http://subversion.tigris.org/servlets/ReadMsg?list=svn&msgNo=29642
-	# matches are performed in a case-insensitive manner
+        # see http://subversion.tigris.org/servlets/ReadMsg?list=svn&msgNo=29642
+        # matches are performed in a case-insensitive manner
 
-	my ($glob, $autoprops);
-	while (($glob, $autoprops) = each %{ $autoprops_list }) {
-	    my $entry = ();
-	    my $regex = Text::Glob::glob_to_regex_string($glob);
-	    $entry->{glob} = qr/$regex/i;
-	    $entry->{props} = $autoprops;
-	    push @{$self->{entries}}, $entry; 
-	}
+        my ($glob, $autoprops);
+        while (($glob, $autoprops) = each %{ $autoprops_list }) {
+            my $entry = ();
+            my $regex = Text::Glob::glob_to_regex_string($glob);
+            $entry->{glob} = qr/$regex/i;
+            $entry->{props} = $autoprops;
+            push @{$self->{entries}}, $entry; 
+        }
     }
    
     $self = bless($self, $class);
